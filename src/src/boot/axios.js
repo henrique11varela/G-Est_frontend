2f5fe31e279740eb0f376e952423d491b347612c
Henrique Varela <henrique11varela@gmail.com>
2024-01-05 02:15:46 +0000

    axios and .env settings

--- src/src/boot/axios.js
+++ src/src/boot/axios.js
@@ -1,13 +1,15 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
+axios.defaults.withCredentials = true
+
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
 // If any client changes this (global) instance, it might be a
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' })
+const api = axios.create({ baseURL: `${import.meta.env.VITE_API_URL}:${import.meta.env.VITE_API_PORT}/api/v1/` })
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
