fa1a0c4d35a2fb5688faf99975b8d01a57216412
henrique11varela <henrique11varela@gmail.com>
2024-01-15 14:58:09 -0800

    pagination dto fix

--- src/src/dto/PaginationDTO.js
+++ src/src/dto/PaginationDTO.js
@@ -6,8 +6,8 @@ function input(data) {
   try {
     return {
       total: data.total,
-      currentPage: data.currentPage,
-      perPage: data.perPage,
+      currentPage: data.current_page,
+      perPage: data.per_page,
     }
   } catch (error) {
     console.error("Error:", error);
