9e1d283e6834a9c803c705afbb55be4df7c2066d
Pedro Pereira <pedroffpereira2001@gmail.com>
2024-02-01 23:33:34 +0000

    add tutors and contact on dto

--- src/src/dto/CompanyDTO.js
+++ src/src/dto/CompanyDTO.js
@@ -16,7 +16,22 @@ function input(data) {
         peoples.push(new CompanyPeopleDTO.input(people))
       }
     }
-
+    const tutors = []
+    let hasTutorsKey = false
+    if (data.hasOwnProperty('tutor_people')) {
+      hasTutorsKey = true
+      for (const people of data.tutor_people) {
+        tutors.push(new CompanyPeopleDTO.input(people))
+      }
+    }
+    const contacts = []
+    let hasContactsKey = false
+    if (data.hasOwnProperty('contact_people')) {
+      hasContactsKey = true
+      for (const people of data.contact_people) {
+        contacts.push(new CompanyPeopleDTO.input(people))
+      }
+    }
 
     const addresses = []
     let hasAddressesKey = false
@@ -26,14 +41,18 @@ function input(data) {
         addresses.push(new CompanyAddressDTO.input(people))
       }
     }
-    console.log(addresses);
     return {
       id: data.id,
       name: data.name,
       niss: data.niss,
       nipc: data.nipc,
+      numberPeople: peoples.length,
+      numberTutor: tutors.length,
+      numberContact: contacts.length,
       ...(hasPeopleKey && { peoples }),
-      ...(hasAddressesKey && {addresses})
+      ...(hasAddressesKey && {addresses}),
+      ...(hasTutorsKey && {tutors}),
+      ...(hasContactsKey && {contacts})
     }
   } catch (error) {
     console.error("Error:", error)
