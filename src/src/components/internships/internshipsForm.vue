9db68fa61e91f007f167df79ea42f22cd1b3aedc
henrique11varela <henrique11varela@gmail.com>
2024-02-01 00:08:59 +0000

    Refactor form to help the user

--- src/src/components/internships/internshipsForm.vue
+++ src/src/components/internships/internshipsForm.vue
@@ -10,11 +10,11 @@
 
       <div ref="companiesElement">
         <!-- first -->
-        <div v-for="numberSelect in selectedCompanies.length" :key="numberSelect" class="row">
+        <div v-for="numberSelect in internshipData.companies.length" :key="numberSelect" class="row">
           <div class="q-mb-md col-7">
-            <q-select outlined label="Empresa" v-model="selectedCompanies[numberSelect - 1].company" use-input
-              hide-selected fill-input input-debounce="500" :options="options.companies" option-label="name"
-              @filter="filterCompaniesFn">
+            <q-select outlined label="Empresa" v-model="internshipData.companies[numberSelect - 1].company" use-input
+              hide-selected fill-input input-debounce="500" :options="options.companies" :readonly="isAccepted || pageState.isSubmitting"
+              :loading="pageState.loadingCompanies" option-label="name" @filter="filterCompaniesFn">
               <template v-slot:no-option>
                 <q-item>
                   <q-item-section class="text-grey">
@@ -25,38 +25,62 @@
             </q-select>
           </div>
           <div class="q-mb-md col q-pl-lg">
-            <q-select outlined label="Estado" v-model="selectedCompanies[numberSelect - 1].status"
-              :options="options.status">
+            <q-select outlined label="Estado" v-model="internshipData.companies[numberSelect - 1].status"
+              :options="options.status"
+              :readonly="(isAccepted && internshipData.companies[numberSelect - 1].status != 'Aceite') || internshipData.companies[numberSelect - 1].company == '' || pageState.isSubmitting">
             </q-select>
           </div>
-          <div v-if="selectedCompanies.length > 1" class="q-mb-md col-1 flex justify-center items-center">
+          <div v-if="internshipData.companies.length > 1 && !isAccepted && !pageState.isSubmitting" class="q-mb-md col-1 flex justify-center items-center">
             <q-btn color="primary" round icon="remove" @click="removeCompany(numberSelect - 1)"></q-btn>
           </div>
         </div>
-        <q-btn color="primary" round class="q-mb-md" icon="add" @click="addCompany"></q-btn>
+        <q-btn color="primary" v-if="!isAccepted && !pageState.isSubmitting" round class="q-mb-md" icon="add" @click="addCompany"></q-btn>
       </div>
-      <!-- <q-btn color="primary" round class="q-mb-md">+</q-btn> -->
 
-      <div v-if="startFormShow">
+
+      <div v-if="pageState.started">
         <hr class="q-mb-md">
-        <q-input outlined v-model="date" mask="date" :rules="['date']">
-          <template v-slot:append>
-            <q-icon name="event" class="cursor-pointer">
-              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                <q-date v-model="date" :locale="qDateLocale">
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup label="Fechar" color="primary" flat />
-                  </div>
-                </q-date>
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
+        <div class="row">
+          <div class="col q-pr-sm">
+            <q-input outlined v-model="internshipData.startedInternship.startingDate" mask="date" :rules="['date']" :readonly="pageState.isSubmitting"
+              label="Data de Inicio">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                    <q-date v-model="internshipData.startedInternship.startingDate" :locale="qDateLocale" :readonly="pageState.isSubmitting">
+                      <div class="row items-center justify-end">
+                        <q-btn v-close-popup label="Fechar" color="primary" flat />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
 
-        <q-checkbox label="Refeição" v-model="refeição" />
+          <div class="col q-pl-sm">
+            <q-input outlined v-model="internshipData.startedInternship.endingDate" mask="date" :rules="['date']" :readonly="pageState.isSubmitting"
+              label="Data de Fim">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                    <q-date v-model="internshipData.startedInternship.endingDate" :locale="qDateLocale" :readonly="pageState.isSubmitting">
+                      <div class="row items-center justify-end">
+                        <q-btn v-close-popup label="Fechar" color="primary" flat />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </div>
 
-        <q-select outlined label="Tutor" v-model="companyPerson" use-input hide-selected fill-input input-debounce="500"
-          :options="options.companies" option-label="name" @filter="filterTutorsFn" class="q-mb-md col-7">
+        <q-checkbox label="Refeição" class="q-mb-md" v-model="internshipData.startedInternship.mealAllowance" :disable="pageState.isSubmitting" />
+
+        <q-select outlined label="Tutor" v-model="internshipData.startedInternship.companyPerson" use-input hide-selected
+          fill-input input-debounce="500" :options="optionsTutors" option-label="name" @filter="filterTutorsFn" :readonly="pageState.isSubmitting"
+          class="q-mb-md col-7">
           <template v-slot:no-option>
             <q-item>
               <q-item-section class="text-grey">
@@ -66,8 +90,9 @@
           </template>
         </q-select>
 
-        <q-select outlined label="Morada" v-model="companyAddress" use-input hide-selected fill-input input-debounce="500"
-          :options="options.companies" option-label="name" @filter="filterMoradaFn" class="q-mb-md col-7">
+        <q-select outlined label="Morada" v-model="internshipData.startedInternship.companyAddress" use-input
+          hide-selected fill-input input-debounce="500" :options="optionsAddresses" option-label="description" :readonly="pageState.isSubmitting"
+          @filter="filterMoradaFn" class="q-mb-md col-7">
           <template v-slot:no-option>
             <q-item>
               <q-item-section class="text-grey">
@@ -76,6 +101,7 @@
             </q-item>
           </template>
         </q-select>
+
         <hr class="q-mb-md">
 
       </div>
@@ -83,7 +109,7 @@
       <q-input outlined class="q-mb-md" v-model="internshipData.observations" label="Observações" type="textarea" />
 
       <q-btn color="primary" type="submit">Salvar</q-btn>
-      <q-btn color="primary" v-if="!startFormShow && pageState.readyStarted" @click="startFormShow = true">Iniciar
+      <q-btn color="primary" v-if="!pageState.started && pageState.readyToBeStarted" @click="startInternship">Iniciar
         estágio</q-btn>
       <!-- {{ internshipData }} -->
     </q-form>
@@ -96,11 +122,9 @@ import { useRoute } from "vue-router";
 import { qDateLocale } from "../../config/config.js";
 import internshipsAPI from "../../services/fetches/internships.js";
 import studentsAPI from "../../services/fetches/students.js";
-import classesAPI from "../../services/fetches/classes.js";
 import companiesAPI from "../../services/fetches/companies.js";
 
 const route = useRoute();
-const startFormShow = ref(false);
 
 // GERAL
 const internshipData = ref({
@@ -112,46 +136,62 @@ const internshipData = ref({
     name: "",
   },
   observations: "",
-  companies: [],
-  startedInternship: null,
+  companies: [
+    {
+      company: "",
+      status: "Opção",
+    },
+  ],
+  startedInternship: {
+    startingDate: "",
+    endingDate: "",
+    mealAllowance: false,
+    companyPerson: "",
+    companyAddress: "",
+  },
   endedInternship: null,
 });
 
 const pageState = ref({
+  isSubmitting: false,
   loading: false,
+  loadingCompanies: false,
   edit: false,
-  readyStarted: false,
+  readyToBeStarted: false,
   started: false,
 });
 
 // Companies colocação
-const selectedCompanies = ref([
-  {
-    company: "",
-    status: "",
-  }
-]);
 
 const isAccepted = computed(() => {
-  return selectedCompanies.value.some(e => e.status === 'Aceite');
+  return internshipData.value.companies.some(e => e.status === 'Aceite');
+});
+
+const acceptedCompany = computed(() => {
+  return internshipData.value.companies.find(e => e.status === 'Aceite').company;
 });
 
 watch(
-  () => selectedCompanies,
+  () => internshipData.value.companies,
   (val) => {
-    pageState.value.readyStarted = isAccepted.value
+    if (pageState.value.started && val.every((item) => item.status != 'Aceite')) {
+      internshipData.value.startedInternship.companyAddress = '';
+      internshipData.value.startedInternship.companyPerson = '';
+    }
+    pageState.value.readyToBeStarted = isAccepted.value
   },
-  { deep: true });
+  { deep: true }
+);
 
 function addCompany() {
-  selectedCompanies.value.push({
+  internshipData.value.companies.value.push({
     company: '',
-    status: ''
+    status: 'Opção'
   })
 }
 
 function removeCompany(id) {
-  selectedCompanies.value.splice(id, 1);
+  internshipData.value.companies.value.splice(id, 1);
 }
 
 const options = ref({
@@ -159,21 +199,19 @@ const options = ref({
   companies: [],
 });
 
+const optionsTutors = ref([]);
+
+const optionsAddresses = ref([]);
+
 // started
 
-const startedInternship = ref({
-  date: "",
-  refeição: false,
-  companyPerson: "",
-  companyAddress: "",
-});
+function startInternship() {
+  pageState.value.started = true;
+}
 
-const endedInternship = ref({
-  date: "",
-  refeição: false,
-  companyPerson: "",
-  companyAddress: "",
-});
+// ended
+
+
 
 // fetches
 async function fetchInternship() {
@@ -182,7 +220,6 @@ async function fetchInternship() {
     student_collection_id: route.params.class,
     student_id: route.params.student,
   })
-
   if (data.data.length > 0) { //If there is any internship
     const internship = data.data.find((item) => {
       return item.endedInternship === null;
@@ -197,7 +234,7 @@ async function fetchInternship() {
   }
   if (!pageState.value.edit) {
     internshipData.value.student = await studentsAPI.show(route.params.student);
-    internshipData.value.class = await classesAPI.show(route.params.class);
+    internshipData.value.class = internshipData.value.student.classes.find((item) => item.id == route.params.class)
   }
   pageState.value.loading = false;
 }
@@ -208,19 +245,22 @@ onMounted(() => {
 });
 
 function onSubmit() {
-  // console.log(selectedCompanies.value);
-  console.log(test.value);
+  pageState.value.isSubmitting = true;
+  console.log(internshipData.value);
+  // SUBMIT LOGIC
+  pageState.value.isSubmitting = false;
 }
 
 // filters
 function filterCompaniesFn(val, update, abort) {
   update(async () => {
+    pageState.value.loadingCompanies = true;
     const needle = val.toLowerCase()
     const response = await companiesAPI.index({
       name: needle,
     })
     options.value.companies = response.data
-    console.log(options.value.companies);
+    pageState.value.loadingCompanies = false;
   })
 }
 
@@ -237,15 +277,12 @@ function filterTutorsFn(val, update, abort) {
 }
 
 function filterMoradaFn(val, update, abort) {
-  // update(async () => {
-  //   const needle = val.toLowerCase()
-  //   const listOfTutors = internshipData.value.companies.find((item) => {
-  //     return item.company.id === companyPerson;
-  //   }).tutors;
-  //   // .filter((item) => {
-  //   //   return item.name.toLowerCase().indexOf(needle) > -1;
-  //   // })
-  // })
+  update(async () => {
+    const needle = val.toLowerCase()
+    optionsAddresses.value = acceptedCompany.value.addresses.filter((item) => {
+      return item.description.toLowerCase().indexOf(needle) > -1 || item.address.toLowerCase().indexOf(needle) > -1;
+    })
+  })
 }
 
 </script>
