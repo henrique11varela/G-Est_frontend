55c36f901477f99f47e0a684fe87a53b611bd351
Vasco Silva <vasco.silva.t0123187@edu.atec.pt>
2024-02-06 11:42:33 +0000

    Course crud

---
+++ src/src/pages/Courses/CoursesAdd.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding>
+    <div class="q-pa-md">
+      <h1 class="text-h6">Criar curso</h1>
+      <CoursesForm @valuecreated="postSubmit"></CoursesForm>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import CoursesForm from 'src/components/courses/CoursesForm.vue'
+import { useRouter } from 'vue-router'
+import notify from 'src/composables/notify'
+const router = useRouter()
+
+function postSubmit(value) {
+  notify.store()
+  router.push('/courses')
+}
+
+</script>
