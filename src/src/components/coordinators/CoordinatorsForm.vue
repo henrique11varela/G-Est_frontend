72cb182bd915b262bb883f905b3358cb0e63e4f1
José Almeida <jose.almeida.t0123955@edu.atec.pt>
2024-02-22 23:57:20 +0000

    Update filter parameter in ApplicationsList.vue and CoordinatorsEdit.vue

--- src/src/components/coordinators/CoordinatorsForm.vue
+++ src/src/components/coordinators/CoordinatorsForm.vue
@@ -1,64 +1,88 @@
 <template>
-    <div class="q-py-md">
-    <q-spinner
-      color="primary"
-      size="3em"
-      :thickness="2"
-      v-if="loading"
-    />
-    <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md row"
-      v-else
-    >
-      <q-input
-        outlined
-        v-model="data.name"
-        label="Nome"
-        lazy-rules="ondemand"
-        :rules="rules.name"
-        class="col-12 col-sm-auto"
-      />
-      <q-input
-        outlined
-        v-model="data.emailAtec"
-        label="Email"
-        lazy-rules="ondemand"
-        :rules="rules.email"
-        class="col-12 col-sm-auto"
-      />
-      <q-input
-        outlined
-        v-model="data.phoneNumber"
-        label="Telefone"
-        lazy-rules="ondemand"
-        :rules="rules.phoneNumber"
-        class="col-12 col-sm-auto"
-      />
+  <div class="q-py-md">
+    <q-spinner color="primary" size="3em" :thickness="2" v-if="loading" />
+    <q-form @submit="onSubmit" @reset="onReset" class="q-ma-lg row" v-else>
+      <div class="col-12 row">
+        <q-input class="col q-mr-sm"
+          :readonly="!loginStore.isAdmin"
+          outlined
+          v-model="data.name"
+          label="Nome"
+          lazy-rules="ondemand"
+          :rules="rules.name"
 
+        />
+        <q-input class="col q-mx-sm"
+          :readonly="!loginStore.isAdmin"
+          outlined
+          v-model="data.emailAtec"
+          label="Email"
+          lazy-rules="ondemand"
+          :rules="rules.email"
 
+        />
+        <q-input class="col q-ml-sm"
+          :readonly="!loginStore.isAdmin"
+          outlined
+          v-model="data.phoneNumber"
+          label="Telefone"
+          lazy-rules="ondemand"
+          :rules="rules.phoneNumber"
+
+        />
+      </div>
+
+      <div v-if="loginStore.isAdmin" class="col-12">
+        <q-btn
+          unelevated
+          label="Guardar"
+          type="submit"
+          color="primary"
+          :disabled="submitting"
+        />
+        <q-btn
+          unelevated
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+          :disabled="submitting"
+        />
+        <q-spinner
+          color="primary"
+          size="2.5em"
+          :thickness="2"
+          v-if="submitting"
+        />
+      </div>
       <div class="col-12">
-        <q-btn unelevated label="Guardar" type="submit" color="primary" :disabled="submitting"/>
-        <q-btn unelevated label="Reset" type="reset" color="primary" flat class="q-ml-sm" :disabled="submitting"/>
-        <q-spinner color="primary" size="2.5em" :thickness="2" v-if="submitting"/>
+        <hr />
+        <q-btn
+          type="submit"
+          label="Voltar"
+          color="primary"
+          to="/coordinators"
+        />
       </div>
     </q-form>
-    </div>
+  </div>
 </template>
 
 <script setup>
-import CoordinatorDTO from '../../dto/CoordinatorDTO'
-import { ref, onMounted, watch } from 'vue'
-import coordinatorsAPI from '../../services/fetches/coordinators'
-import { useRoute } from "vue-router"
+import CoordinatorDTO from "../../dto/CoordinatorDTO";
+import { ref, onMounted, watch } from "vue";
+import coordinatorsAPI from "../../services/fetches/coordinators";
+import { useRoute } from "vue-router";
+import { useLoginStore } from "../../stores/login.js";
 
-const data = ref(defaultValues())
-const defaults = defaultValues()
-const loading = ref(false)
-const submitting = ref(false)
-const rules = CoordinatorDTO.rules()
+const data = ref(defaultValues());
+const defaults = defaultValues();
+const loading = ref(false);
+const submitting = ref(false);
+const rules = CoordinatorDTO.rules();
 const route = useRoute();
+const loginStore = useLoginStore();
 
 const props = defineProps({
   edit: Boolean,
@@ -66,50 +90,49 @@ const props = defineProps({
 
 onMounted(async () => {
   try {
-    if (props.edit)
-    {
-      loading.value = true
-      getCoordinator(route.params.id)
-      loading.value = false
+    if (props.edit) {
+      loading.value = true;
+      getCoordinator(route.params.id);
+      loading.value = false;
     }
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 
 watch(
   () => route.params.id,
-  async newId => getCoordinator(newId)
-)
+  async (newId) => getCoordinator(newId)
+);
 
 async function getCoordinator(id) {
-  const output = await coordinatorsAPI.show(id)
-  data.value.name = defaults.name = output.name
-  data.value.emailAtec = defaults.emailAtec = output.emailAtec
-  data.value.phoneNumber = defaults.phoneNumber = output.phoneNumber
-  data.value.id = id
+  const output = await coordinatorsAPI.show(id);
+  data.value.name = defaults.name = output.name;
+  data.value.emailAtec = defaults.emailAtec = output.emailAtec;
+  data.value.phoneNumber = defaults.phoneNumber = output.phoneNumber;
+  data.value.id = id;
 }
 
 function defaultValues() {
-  return { name: "", emailAtec: "", phoneNumber: ""}
+  return { name: "", emailAtec: "", phoneNumber: "" };
 }
 
 function onReset() {
-  data.value.name = defaults.name
-  data.value.emailAtec = defaults.emailAtec
-  data.value.phoneNumber = defaults.phoneNumber
+  data.value.name = defaults.name;
+  data.value.emailAtec = defaults.emailAtec;
+  data.value.phoneNumber = defaults.phoneNumber;
 }
 
-const emit = defineEmits(['valuecreated'])
+const emit = defineEmits(["valuecreated"]);
 
 async function onSubmit() {
-  submitting.value = true
+  submitting.value = true;
 
-  const output = props.edit ?
-  await coordinatorsAPI.update(data.value) :
-  await coordinatorsAPI.store(data.value)
+  const output = props.edit
+    ? await coordinatorsAPI.update(data.value)
+    : await coordinatorsAPI.store(data.value);
 
-  submitting.value = false
-  emit('valuecreated', output)
+  submitting.value = false;
+  emit("valuecreated", output);
 }
 </script>
