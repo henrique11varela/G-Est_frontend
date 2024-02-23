42fab7ca6bd37a104a804359936ca053a2dc5b2d
Vasco Silva <vasco.silva.t0123187@edu.atec.pt>
2024-02-23 04:03:59 +0000

    Current internship status

--- src/src/components/classes/CurrentInternship.vue
+++ src/src/components/classes/CurrentInternship.vue
@@ -1,33 +1,45 @@
 <template>
   <q-td>
-    <div :key="index" v-for="(internship, index) in props.internships">
-      {{ console.log(internship) }}
-      <div>{{ internship.id }}</div>
+    <div class="column q-gutter-xs">
+      <div :key="index" v-for="(internship, index) in props.internships">
+        <router-link :to="`/internships/${internship.id}`" class="link">{{ setInternshipStatus(internship) }}</router-link>
+      </div>
     </div>
   </q-td>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-
 const props = defineProps({
-  internships: Object
+  internships: {
+    type: Array,
+    required: true,
+  }
 })
 
-const currentInternship = ref({ company: "", status: "", date: "" })
-
-// onMounted(() => compareInternships())
-
-function setCurrentInternship() {
-  for (const internship of props.tableData.row.internships) {
-    for (const company of internship.companies) {
-      if (company.pivot.status === 'Em Colocação') {
-        currentInternship.value.company = company.name
-        currentInternship.value.status = company.pivot.status
-        break
-      }
-    }
+function setInternshipStatus(internship) {
+  if (internship.endedInternship) {
+    const company = internship.companies.find(c => c.status === 'Aceite')
+    const append = internship.endedInternship.reason
+    return `${company?.name} - Terminado (${append})`
   }
-}
 
+  if (internship.startedInternship) {
+    const company = internship.companies.find(c => c.status === 'Aceite')
+    const now = new Date()
+    const startDate = new Date(internship.startedInternship.startDate)
+    const append = startDate <= now ? 'Iniciado' : 'Colocado'
+    return `${company?.name} - ${append}`
+  }
+
+  let company = internship.companies.find(c => c.status === 'Em Colocação')
+  if (!company) company = internship.companies.find(c => c.status === 'Opção')
+  if (!company) company = internship.companies.find(c => c.status === 'Não Aceite')
+  return `${company?.name} - ${company?.status}`
+}
 </script>
+
+<style scoped>
+.link {
+  text-decoration: none;
+}
+</style>
