daf803515675275476dd59f53b5f950ebc69ba07
henrique11varela <henrique11varela@gmail.com>
2024-02-28 15:30:24 +0000

    Add delete button to InternshipsPage.vue

--- src/src/pages/Internships/InternshipsPage.vue
+++ src/src/pages/Internships/InternshipsPage.vue
@@ -1,14 +1,30 @@
 <template>
   <q-page padding>
-    <h1 class="text-h6">Estágio</h1>
+    <div class="flex justify-between items-center">
+      <h1 class="text-h6">Estágio</h1>
+      <div>
+        <q-btn unelevated color="negative" label="Apagar" @click="deleteClass"
+          v-if="loginStore.isAdmin && !!route.params.id" />
+      </div>
+    </div>
     <InternshipsForm />
   </q-page>
 </template>
 
 <script setup>
-  import InternshipsForm from "../../components/internships/internshipsForm.vue";
+import InternshipsForm from "../../components/internships/internshipsForm.vue";
+import internshipsAPI from "../../services/fetches/internships.js";
+import { useRoute, useRouter } from 'vue-router'
+import deleteModel from 'src/composables/delete'
+import { useLoginStore } from 'src/stores/login'
+const loginStore = useLoginStore()
+
+const router = useRouter()
+const route = useRoute()
+
+function deleteClass() {
+  deleteModel(internshipsAPI.destroy, route.params.id, router, 'o estágio')
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
